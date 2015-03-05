dd1e782f3efc5c6af0af4800e07fba54d670f689
JimJarvis <jimfanspire@gmail.com>
2015-03-05 16:17:17 -0500

    test.cpp -> test.cu

--- test.cu
+++ test.cu
@@ -9,22 +9,29 @@
 
 int main(int argc, char **argv)
 {
-#if __cplusplus > 201100L
+#ifdef is_CPP_11
 	cout << "C++ 11 supported!" << endl;
 #else
 	cout << "C++ 11 NOT supported!" << endl;
 #endif
 
+#ifdef is_CUDA
+	cout << "CUDA supported!" << endl;
+#else
+	cout << "CUDA NOT supported!" << endl;
+#endif
+
 
 	double p = 66;
-	CpuTimer t;
+	GpuTimer t;
 	t.start();
 	for (int i = 0; i < 10000000; ++i)
 		p += i / p - std::sqrt(p);
 
 	t.setResolution(Timer::Millisec).printElapsed();
 
-	vector<int> a {1, 2, 3, 4, 5};
+	vector<int> a;
+	a.push_back(5); a.push_back(1); a.push_back(4); a.push_back(9); a.push_back(0);
 	cout << a << endl;
 	cout << p << endl;
 }
