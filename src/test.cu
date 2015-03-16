5eb002722db67f982df90b37094c54edd7a60534
Jim Fan <jimfanspire@gmail.com>
2015-03-15 21:51:59 -0400

    More sophisticated test

--- src/test.cu
+++ src/test.cu
@@ -8,12 +8,34 @@
 #include "mytimer.h"
 #include "fileutils.h"
 
+#ifdef is_CUDA
+__global__ void testkernel()
+{
+	double p = threadIdx.x + 66;
+	for (int i = 0; i < 30000000; ++i)
+		p += i / p - std::sqrt(p);
+
+	printf("thread %d; block %d\n", threadIdx.x, blockIdx.x);
+}
+#endif
+
 int main(int argc, char **argv)
 {
 #ifdef is_CPP_11
 	cout << "C++ 11 supported!" << endl;
 #else
 	cout << "C++ 11 NOT supported!" << endl;
+#endif
+#ifdef is_CPP_11
+	vector<int> a {115, 113, 111, 110, 112};
+	cout << "[";
+	for (auto i : a)
+		cout << i << " ";
+	cout << "]";
+#else
+	vector<int> a;
+	a.push_back(5); a.push_back(1); a.push_back(4); a.push_back(9); a.push_back(0);
+	cout << a << endl;
 #endif
 
 #ifdef is_CUDA
@@ -22,18 +44,13 @@ int main(int argc, char **argv)
 	cout << "CUDA NOT supported!" << endl;
 #endif
 
-
+#ifdef is_CUDA
 	double p = 66;
 	GpuTimer t;
 	t.start();
-	for (int i = 0; i < 30000000; ++i)
-		p += i / p - std::sqrt(p);
+	testkernel<<< 3, 4 >>>();
+	t.setResolution(Timer::Microsec).printElapsed();
+#endif
 
-	t.setResolution(Timer::Sec).printElapsed();
-
-	vector<int> a;
-	a.push_back(5); a.push_back(1); a.push_back(4); a.push_back(9); a.push_back(0);
-	cout << a << endl;
-
-	FileReader<uint> reader("shit");
+	FileReader<uint> reader("nothing");
 }
